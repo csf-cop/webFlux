2e1b6b3e346d8dcb97fecc8e847a2d6e83dc400c
Dang Quang Tuan <quangtuanhm@gmail.com>
2019-07-14 09:36:57 +0700

    Add angular project.

--- csf/whoami-security/src/main/java/com/csf/whoami/security/config/SecurityConfig.java
+++ csf/whoami-security/src/main/java/com/csf/whoami/security/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.csf.whoami.security.config;
 
-import com.csf.whoami.security.repository.AppUserRepository;
-import com.csf.whoami.security.service.DefaultUserDetailsService;
+import javax.annotation.Resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -9,8 +8,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.csf.whoami.security.repository.AppUserRepository;
+import com.csf.whoami.security.service.DefaultUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
@@ -29,5 +34,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(new DefaultUserDetailsService(userRepository));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/api-docs/**").permitAll();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
     }
 }
