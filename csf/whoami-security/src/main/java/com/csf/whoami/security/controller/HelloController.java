8bf3914a5dd57b80d6870ce1601549130d2470e7
Tuan Dang Q <tuan.dang@asiantech.vn>
2019-07-17 10:56:49 +0700

    Add source code check anonymous (postman view).

--- csf/whoami-security/src/main/java/com/csf/whoami/security/controller/HelloController.java
+++ csf/whoami-security/src/main/java/com/csf/whoami/security/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,5 +29,10 @@ public class HelloController {
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     public Welcome greetings(@RequestParam("name") String name, Principal principal) {
         return new Welcome(name + " (" + principal.getName() + ")" + TokenUtils.extractCurrentToken());
+    }
+
+    @GetMapping("/test")
+    public String testMethod() {
+    	return "test anonymous";
     }
 }
