d7723b67fddc145200a1e71202a02047b5aaf521
Đặng Quang Tuấn <tuan.dang@asiantech.vn>
2019-07-22 17:06:34 +0700

    Demo Spring boot oauth2 JWT Social network login.

---
+++ csf/whoami-security/src/main/java/com/csf/whoami/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.csf.whoami.security;
+
+
+import com.csf.whoami.exception.ResourceNotFoundException;
+import com.csf.whoami.model.User;
+import com.csf.whoami.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by rajeevkumarsingh on 02/08/17.
+ */
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email : " + email)
+        );
+
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(Long id) {
+        User user = userRepository.findById(id).orElseThrow(
+            () -> new ResourceNotFoundException("User", "id", id)
+        );
+
+        return UserPrincipal.create(user);
+    }
+}
